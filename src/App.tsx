1076c737bbc26e21fe0772a431b6b0a0d5ef0c0a
mudassar066 <mudassar.786.mhr@gmail.com>
2026-05-20 21:36:26 +0500

    Removed old imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 function App() {
   return (
-    <div style={{padding: "40px", fontFamily: "Arial"}}>
+    <div>
       <h1>Marvion Marketing</h1>
-      <p>Website Successfully Deployed</p>
     </div>
   )
 }
